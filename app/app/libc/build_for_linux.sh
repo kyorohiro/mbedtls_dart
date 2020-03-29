13306ea6548fa31677716e539219a0d73ce12b4a
kyorohiro <kyorohiro@gmail.com>
2020-03-29 13:20:44 +0900

    add build script

--- app/app/libc/build_for_linux.sh
+++ app/app/libc/build_for_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#gcc -c -Wall -Werror -fpic md5.c
+
+find . -name "*.o" | xargs rm
 gcc -Wall -Werror -fpic -I. -c buffer.c -o buffer.o 
 gcc -Wall -Werror -fpic -I. -c md5.c -o md5.o
 gcc -shared -o libmd5.so md5.o buffer.o /usr/local/lib/libmbedcrypto.a 
